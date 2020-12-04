01317782a58a15b077050a57eb45f987d3feecb5
Jip <jhp.moors@gmail.com>
2020-12-04 15:03:01 +0100

    Day 1: Part 2

--- index.js
+++ index.js
@@ -3,15 +3,30 @@ var fs = require('fs');
 const data = fs.readFileSync('input.list', 'utf8');
 const list = data.split('\n').map((item) => parseInt(item, 10));
 
-let result;
+let result2;
 
 for (let i = 0; i < list.length - 1; i++) {
     for (let j = 1; j < list.length - 1; j++) {
         if (list[i] + list[j] === 2020) {
-            result = list[i] * list[j];
+            result2 = list[i] * list[j];
             break;
         }
     }
 }
 
-console.log(result);
+console.log(result2);
+
+let result3;
+
+for (let i = 0; i < list.length - 3; i++) {
+    for (let j = 1; j < list.length - 2; j++) {
+        for (let z = 2; z < list.length - 1; z++) {
+            if (list[i] + list[j] + list[z] === 2020) {
+                result3 = list[i] * list[j] * list[z];
+                break;
+            }
+        }
+    }
+}
+
+console.log(result3);
