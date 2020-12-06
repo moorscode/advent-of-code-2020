c8ffa763d0bbb99be87c7c76e4d70b98e6595833
Jip Moors <jhp.moors@gmail.com>
2020-12-06 09:52:51 +0100

    Day 6: Part 2

--- day6.js
+++ day6.js
@@ -6,10 +6,16 @@ const groups = data.split('\n\n').map( (group) => {
     const persons = group.split('\n');
     const choices = persons.map( (person) => person.split('') );
 
-    return [].concat.apply( [], choices ).filter((v, i, a) => a.indexOf(v) === i);
-});
+    const all = choices.reduce( (previous, current) => {
+        if(previous === false) {
+            return current;
+        }
+        return previous.filter((x)=>current.includes(x));
+    }, false );
+
+    return all;
+}).filter((x)=>x);
 
 const sum = groups.map( ( group ) => group.length ).reduce((a,b) => a + b, 0);
 
-// console.log(groups);
 console.log(sum)
