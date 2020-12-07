c022bf990fa2ff4b6f08bae0620ed5c4b63215b1
Jip Moors <jhp.moors@gmail.com>
2020-12-07 15:41:10 +0100

    Day 7: Part 2

--- day7.js
+++ day7.js
@@ -15,10 +15,10 @@ const rules = data.split('\n')
             inside = insideMatch.map((matched) => {
                 // Strip starting space and ending comma or dot.
                 // Extract number and name.
-                return matched.substr(3, matched.length - 4).replace(/\sbags?/, '');
+                // return matched.substr(3, matched.length - 4).replace(/\sbags?/, '');
                 return {
                     count: parseInt(matched.substr(1, 1), 10),
-                    bag: matched.substr(3, matched.length - 4)
+                    bag: matched.substr(3, matched.length - 4).replace(/\sbags?/, '')
                 };
             });
         }
@@ -28,44 +28,16 @@ const rules = data.split('\n')
         return prev;
     }, {});
 
-// how many can handle a shiny gold bag.
-// which can handle one directly.
-// which can handle these ^ directly (recusive)
-// assuming no infinite loops..
-
-// follow inside until inside is empty or bag is found.
-function canContainBag(list, bag, count, fullList) {
-    for (const [key, value] of Object.entries(list) ) {
-        if (value.length === 0) {
-            continue;
-        }
-
-        // Looking for this!
-        if (value.includes( bag ) ) {
-            count += 1;
-        }
-
-        // Recursion..
-        for (const bagName of value) {
-            count = canContainBag(
-                { [bagName]: fullList[bagName] },
-                bag,
-                count,
-                fullList
-            );
+function countBags(bag) {
+    let count = 0;
+    if (rules[bag].length > 0) {
+        for (const index in rules[bag]) {
+            const subCount = rules[bag][index].count;
+            count += subCount + subCount * countBags(rules[bag][index].bag);
         }
     }
 
     return count;
-};
+}
 
-const bags = Object.keys(rules).filter((rule) => {
-    return canContainBag({ [rule]: rules[rule] },
-        'shiny gold',
-        0,
-        rules) > 0;
-});
-
-// console.log(Object.keys(rules).length);
-// console.log(canContainBag(rules, 'shiny gold', 0, rules))
-console.log(bags, bags.length);
+console.log(countBags('shiny gold'));
