b543f0f1fd6366a637fea43eeb2ed02919549cd2
Jip <jhp.moors@gmail.com>
2020-12-08 20:39:48 +0100

    Day 8: Part 2

--- day8.js
+++ day8.js
@@ -8,26 +8,74 @@ const commands = data.split('\n').map((row) => {
     return [items[0], items[1].substr(0, 1) === '-' ? 0 - value : value];
 });
 
-let stepper = 0;
-let accumulator = 0;
-let indexHistory = [];
 
-untilforever: while (true) {
-    if (indexHistory.includes(stepper)) {
-        break;
+function run(list) {
+    let stepper = 0;
+    let accumulator = 0;
+    let indexHistory = [];
+
+    untilforever: while (true) {
+        if (stepper === list.length) {
+            console.log('done!');
+            break;
+        }
+
+        if (indexHistory.includes(stepper)) {
+            return indexHistory;
+        }
+
+        indexHistory.push(stepper);
+
+        switch (list[stepper][0]) {
+            case 'jmp':
+                stepper += list[stepper][1];
+                continue untilforever;
+            case 'acc':
+                accumulator += list[stepper][1];
+        }
+
+        stepper += 1;
     }
-    indexHistory.push(stepper);
 
-    switch (commands[stepper][0]) {
-        case 'jmp':
-            stepper += commands[stepper][1];
-            continue untilforever;
-        case 'acc':
-            accumulator += commands[stepper][1];
-    }
-
-    stepper += 1;
+    return accumulator;
 }
 
-console.log(indexHistory[indexHistory.length - 1]);
+let accumulator = -1;
+
+let modifiedCommands = [...commands];
+let stack;
+
+restest: while (true) {
+
+    let result = run(modifiedCommands);
+    if (typeof (result) !== 'object') {
+        accumulator = result;
+        break;
+    }
+
+    if (!stack) {
+        stack = result;
+    }
+
+    // Make a copy of the commands to start fresh.
+    modifiedCommands = [...commands];
+
+    while (stack.length > 0) {
+        let index = stack.pop();
+        switch (commands[index][0]) {
+            case 'acc':
+                continue;
+            case 'jmp':
+                modifiedCommands[index][0] = 'nop';
+                continue restest;
+            case 'nop':
+                modifiedCommands[index][0] = 'jmp';
+                continue restest;
+        }
+    }
+
+    console.log('We shouldnt get here...');
+    break;
+}
+
 console.log(accumulator);
