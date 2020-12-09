1021a4e8f079af033dc4f1d422b93bfd8a26adc5
Jip <jhp.moors@gmail.com>
2020-12-09 20:19:36 +0100

    Day 9: Part 2

--- day9.js
+++ day9.js
@@ -15,14 +15,34 @@ function oneOf(check, list) {
     return possible.includes(check);
 }
 
+let invalid;
+let original = [...numbers];
+
 let previous = numbers.splice(0, 25);
 while (numbers.length > 0) {
     if (!oneOf(numbers[0], previous)) {
-        console.log(numbers[0]);
+        invalid = numbers[0];
         break;
     }
 
     let first = numbers.shift();
     previous.push(first);
     previous.shift();
+}
+
+console.log(invalid);
+
+main: for (let i = 0; i < original.length; i++) {
+    let sum = 0;
+    const list = [];
+    for (let j = i; j < i + original.length; j++) {
+        sum += original[j];
+        list.push(original[j]);
+        if (sum === invalid) {
+            let lowest = Math.min.apply(Math, list);
+            let highest = Math.max.apply(Math, list);
+            console.log(lowest + highest);
+            break main;
+        }
+    }
 }
