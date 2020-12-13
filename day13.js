5baf6b644997bb628d48c0d9fbd07d6342eecec1
Jip Moors <jhp.moors@gmail.com>
2020-12-13 10:25:13 +0100

    Day 13: Part 1

--- day13.js
+++ day13.js
@@ -1,4 +1,14 @@
 var fs = require( "fs" );
 
 // Prep data.
-const data = fs.readFileSync( "day12.list.txt", "utf8" ).split( "\n" );
+const data = fs.readFileSync( "day13.list.txt", "utf8" ).split( "\n" );
+const time = parseInt( data[ 0 ], 10 );
+const busses = data[ 1 ].split( "," ).filter( ( x )=> x !== "x" ).map( ( bus ) => parseInt( bus, 10 ) );
+
+const times = busses.map( ( bus ) => {
+	return { bus, wait: bus - time % bus };
+} ).sort( ( a, b ) => a.wait - b.wait );
+
+const first = Object.entries( times )[ 0 ][ 1 ];
+
+console.log( first.bus * first.wait );
