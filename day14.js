c20f7cd04486d4f3493c573a104cc5a6688ac2d0
Jip Moors <jhp.moors@gmail.com>
2020-12-14 22:03:44 +0100

    Day 14: Part 2

--- day14.js
+++ day14.js
@@ -10,7 +10,7 @@ const masks = data.split( "mask = " ).map( ( item ) => {
 		mask,
 		items: items.filter( ( x ) => x ).map( ( row ) => {
 			const result = row.match( /mem\[(\d+)\]\s=\s(\d+)/ );
-			return { memory: parseInt( result[ 1 ], 10 ), value: parseInt( result[ 2 ], 10 ).toString( 2 ) };
+			return { memory: parseInt( result[ 1 ], 10 ), value: parseInt( result[ 2 ], 10 ) };
 		} ),
 	};
 } );
@@ -30,6 +30,49 @@ function calculate( mask, value ) {
 
 const memory = [];
 
+function go( memoryMask, value ) {
+	if ( ! memoryMask.includes( "X" ) ) {
+		memory[ parseInt( memoryMask.join( "" ), 2 ) ] = value;
+		return;
+	}
+
+	const Xs = memoryMask.map( ( item, index ) => {
+		return item === "X" ? { item, index } : null;
+	} ).filter( ( x ) => x );
+
+	const last = Xs.pop();
+
+	const a = [ ...memoryMask ];
+	a[ last.index ] = 1;
+	const b = [ ...memoryMask ];
+	b[ last.index ] = 0;
+
+	go( a, value );
+	go( b, value );
+}
+
+
+/**
+ * Writes the value to a memory addres and masked addresses.
+ *
+ * @param {string} mask The mask.
+ * @param {number} memoryAddress The memory address to apply the mask to.
+ * @param {number} value The value to write.
+ */
+function write( mask, memoryAddress, value ) {
+	const bitAddress = memoryAddress.toString( 2 );
+	const padded = new Array( mask.length - bitAddress.length + 1 ).join( "0" ) + bitAddress;
+
+	const maskArray = mask.split( "" );
+	const memoryArray = padded.split( "" );
+
+	const memoryMask = maskArray.map( ( item, index ) => {
+		return ( item === "0" ) ? memoryArray[ index ] : item;
+	} );
+
+	go( memoryMask, value );
+}
+
 for ( const maskEntry in masks ) {
 	if ( ! masks[ maskEntry ] ) {
 		continue;
@@ -39,11 +82,15 @@ for ( const maskEntry in masks ) {
 		if ( ! masks[ maskEntry ].items.hasOwnProperty( item ) ) {
 			continue;
 		}
-		memory[ masks[ maskEntry ].items[ item ].memory ] = calculate( mask, masks[ maskEntry ].items[ item ].value );
+		write( mask, masks[ maskEntry ].items[ item ].memory, masks[ maskEntry ].items[ item ].value );
 	}
 }
 
-console.log( memory.reduce( ( accumulator, value ) => {
-	return accumulator + value;
-}, 0 ) );
+console.log( "hier" );
+console.log( memory );
 
+let total = 0;
+for ( const m in memory ) {
+	total += memory[ m ];
+}
+console.log( total );
