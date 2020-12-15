b6b16e761d60618a3686fc4dd50178c1051967b1
Jip <jhp.moors@gmail.com>
2020-12-15 22:14:04 +0100

    Day 15: Part 2

--- day15.js
+++ day15.js
@@ -1,17 +1,39 @@
-// Const data = [ 2, 0, 1, 9, 5, 19 ];
+const time = process.hrtime();
 
-const recent = [ 2, 0, 1, 9, 5, 19 ];
+const indexes = {
+	0: {},
+	1: {},
+	2: {},
+	3: {},
+	4: {},
+	5: {},
+	6: {},
+	7: {},
+	8: {},
+	9: {},
+};
 
-while ( recent.length < 2020 ) {
-	const last = recent[ recent.length - 1 ];
-	const check = [ ...recent ];
-	check.pop();
-	const previousLast = check.lastIndexOf( last );
-	if ( previousLast > -1 ) {
-		recent.push( recent.length - 1 - previousLast );
+const start = [ 2, 0, 1, 9, 5, 19 ];
+const startCount = start.length;
+
+let last = start.pop();
+
+start.forEach( ( item, index ) => {
+	indexes[ item ][ item ] = index;
+} );
+
+for ( let i = startCount; i < 30000000; i++ ) {
+	const a = last;
+
+	const lastKey = last < 10 ? last : last.toString().substr( 0, 1 );
+	if ( last in indexes[ lastKey ] ) {
+		last = i - 1 - indexes[ lastKey ][ last ];
 	} else {
-		recent.push( 0 );
+		last = 0;
 	}
+
+	indexes[ lastKey ][ a ] = i - 1;
 }
 
-console.log( recent[ recent.length - 1 ] );
+console.log( last );
+console.log( process.hrtime( time ) );
