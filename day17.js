eff462f6a09a344afc7b45622b486bd4fbdc9db1
Jip <jhp.moors@gmail.com>
2020-12-17 20:45:10 +0100

    Day 17: Part 2

--- day17.js
+++ day17.js
@@ -1,96 +1,88 @@
-const { Linter } = require( "eslint" );
 const fs = require( "fs" );
 
 // Prep data.
 const data = fs.readFileSync( "day17.list.txt", "utf8" );
 
-// Set initial data on Z = 0;
-// Input[z][y][x];
-let input = [ data.split( "\n" ).map( ( row ) => {
+// Set initial data on W = 0; Z = 0;
+// Input[w][z][y][x];
+let input = [ [ data.split( "\n" ).map( ( row ) => {
 	return row.split( "" );
-} ) ];
+} ) ] ];
 
 const ACTIVE = "#";
 const INACTIVE = ".";
 
-function store( data, value, x, y, z ) {
-	data[ z ] = data[ z ] || [];
-	data[ z ][ y ] = data[ z ][ y ] || [];
+function store( data, value, x, y, z, w ) {
+	data[ w ] = data[ w ] || [];
+	data[ w ][ z ] = data[ w ][ z ] || [];
+	data[ w ][ z ][ y ] = data[ w ][ z ][ y ] || [];
 
-	data[ z ][ y ][ x ] = value;
+	data[ w ][ z ][ y ][ x ] = value;
 }
 
-function retrieve( x, y, z ) {
-	if ( ! input[ z ] || ! input[ z ][ y ] || ! input[ z ][ y ][ x ] ) {
+function retrieve( x, y, z, w ) {
+	if ( ! input[ w ] || ! input[ w ][ z ] || ! input[ w ][ z ][ y ] || ! input[ w ][ z ][ y ][ x ] ) {
 		return INACTIVE;
 	}
-	return input[ z ][ y ][ x ];
+	return input[ w ][ z ][ y ][ x ];
 }
 
-function calculateSurrounding( x, y, z ) {
+function calculateSurrounding( x, y, z, w ) {
 	let count = 0;
 
-	for ( let z1 = -1; z1 <= 1; z1++ ) {
-		for ( let y1 = -1; y1 <= 1; y1++ ) {
-			for ( let x1 = -1; x1 <= 1; x1++ ) {
-				if ( x1 === 0 && y1 === 0 && z1 === 0 ) {
-					continue;
+	for ( let w1 = -1; w1 <= 1; w1++ ) {
+		for ( let z1 = -1; z1 <= 1; z1++ ) {
+			for ( let y1 = -1; y1 <= 1; y1++ ) {
+				for ( let x1 = -1; x1 <= 1; x1++ ) {
+					if ( x1 === 0 && y1 === 0 && z1 === 0 && w1 === 0 ) {
+						continue;
+					}
+					count += retrieve( x + x1, y + y1, z + z1, w + w1 ) === ACTIVE;
 				}
-				count += retrieve( x + x1, y + y1, z + z1 ) === ACTIVE;
 			}
 		}
 	}
 	return count;
 }
 
-const zSize = input.length;
-const ySize = input[ 0 ].length;
-const xSize = input[ 0 ][ 0 ].length;
+const wSize = input.length;
+const zSize = input[ 0 ].length;
+const ySize = input[ 0 ][ 0 ].length;
+const xSize = input[ 0 ][ 0 ][ 0 ].length;
 
+let wOffset = 2;
 let zOffset = 2;
 let xOffset = 2;
 let yOffset = 2;
 
-function show( data ) {
-	for ( let z = 0 - zOffset / 2; z < zSize + zOffset / 2; z++ ) {
-		console.log( "z=" + z );
-		for ( let y = 0 - yOffset / 2; y < ySize + yOffset / 2; y++ ) {
-			const line = [];
-			for ( let x = 0 - xOffset / 2; x < xSize + xOffset / 2; x++ ) {
-				line.push( retrieve( x, y, z ) );
-			}
-			console.log( line.join( "" ) );
-		}
-	}
-}
-
-const result = [];
-
 for ( let a = 0; a < 6; a++ ) {
 	const nextState = [];
 
-	for ( let z = 0 - zOffset / 2; z < zSize + zOffset / 2; z++ ) {
-		for ( let y = 0 - yOffset / 2; y < ySize + yOffset / 2; y++ ) {
-			for ( let x = 0 - xOffset / 2; x < xSize + xOffset / 2; x++ ) {
-				const surrounding = calculateSurrounding( x, y, z );
-				let set = retrieve( x, y, z );
-				switch ( set ) {
-					case ACTIVE:
-						if ( surrounding !== 2 && surrounding !== 3 ) {
-							set = INACTIVE;
-						}
-						break;
-					case INACTIVE:
-						if ( surrounding === 3 ) {
-							set = ACTIVE;
-						}
-						break;
+	for ( let w = 0 - wOffset / 2; w < wSize + wOffset / 2; w++ ) {
+		for ( let z = 0 - zOffset / 2; z < zSize + zOffset / 2; z++ ) {
+			for ( let y = 0 - yOffset / 2; y < ySize + yOffset / 2; y++ ) {
+				for ( let x = 0 - xOffset / 2; x < xSize + xOffset / 2; x++ ) {
+					const surrounding = calculateSurrounding( x, y, z, w );
+					let set = retrieve( x, y, z, w );
+					switch ( set ) {
+						case ACTIVE:
+							if ( surrounding !== 2 && surrounding !== 3 ) {
+								set = INACTIVE;
+							}
+							break;
+						case INACTIVE:
+							if ( surrounding === 3 ) {
+								set = ACTIVE;
+							}
+							break;
+					}
+					store( nextState, set, x, y, z, w );
 				}
-				store( nextState, set, x, y, z );
 			}
 		}
 	}
 
+	wOffset += 2;
 	zOffset += 2;
 	xOffset += 2;
 	yOffset += 2;
@@ -99,18 +91,18 @@ for ( let a = 0; a < 6; a++ ) {
 	input = nextState;
 }
 
-function count( array ) {
+function recursiveCount( array ) {
 	let counter = 0;
 
 	for ( const index in array ) {
 		if ( typeof array[ index ] === "string" ) {
 			counter += array[ index ] === "#" ? 1 : 0;
 		} else {
-			counter += count( array[ index ] );
+			counter += recursiveCount( array[ index ] );
 		}
 	}
 	return counter;
 }
 
 // Console.log( ( JSON.stringify( input ).match( /#/g ) || [] ).length );
-console.log( count( input ) );
+console.log( recursiveCount( input ) );
