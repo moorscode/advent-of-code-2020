d99838f8aba0ef1a05fa9fc04a3a9722c1228d1d
Jip <jhp.moors@gmail.com>
2020-12-19 14:42:35 +0100

    Day 19: Part 2

--- day19.js
+++ day19.js
@@ -6,7 +6,7 @@ let [ rules, subjects ] = data.split( "\n\n" );
 
 rules = rules.split( "\n" ).reduce( ( result, rule ) => {
 	const [ key, value ] = rule.split( ": " );
-	result[ key ] = value;
+	result[ parseInt( key, 10 ) ] = value;
 	return result;
 }, [] );
 
@@ -15,36 +15,47 @@ let foundIndexes = true;
 while ( foundIndexes ) {
 	foundIndexes = false;
 	for ( let r = 0; r < rules.length; r++ ) {
-		for ( let i = 0; i < rules[ r ].length; i++ ) {
-			const rule = rules[ r ];
-			const indexes = rule.match( /(?:[^\d]|^)(\d+)(?:[^\d]|$)/g );
-			console.log( indexes );
-			if ( ! indexes ) {
-				continue;
-			}
+		if ( r === 8 || r === 11 || r === 0 ) {
+			continue;
+		}
 
-			foundIndexes = true;
+		const rule = rules[ r ];
+		const indexes = rule.match( /(?:[^\d]|^)(\d+)(?:[^\d]|$)/g );
+		if ( ! indexes ) {
+			continue;
+		}
 
-			for ( const index in indexes ) {
-				if ( ! /"\d+"/.test( rules[ indexes[ index ].trim() ] ) ) {
-					// Console.log( rules[ r ] );
-					// Console.log( indexes[ index ].trim() );
+		foundIndexes = true;
 
-					let replacement = rules[ indexes[ index ].trim() ];
-					replacement = " " + replacement.replace( /"/g, "" ) + " ";
-					if ( replacement.indexOf( "|" ) !== -1 ) {
-						replacement = " (" + replacement + ") ";
-					}
+		for ( const index in indexes ) {
+			if ( ! /"\d+"/.test( rules[ indexes[ index ].trim() ] ) ) {
+				// Console.log( rules[ r ] );
+				// Console.log( indexes[ index ].trim() );
 
-					// Console.log( replacement );
-					rules[ r ] = rules[ r ].replace( indexes[ index ], replacement );
+				let replacement = rules[ indexes[ index ].trim() ];
+		        replacement = replacement.replace( /"/g, "" );
+				if ( replacement.indexOf( "|" ) !== -1 ) {
+					replacement = "( " + replacement + " )";
 				}
+
+				rules[ r ] = rules[ r ].replace( indexes[ index ], " " + replacement + " " );
 			}
 		}
-		break;
 	}
 }
 
-const theRule = new RegExp( "^" + rules[ 0 ].replace( /\s/g, "" ) + "$", "gm" );
+// 0: 8 11
+// 8: 42 | 42 {8}
+// 11: 42 31 | 42 {11} 31
+rules[ 8 ] = "(" + rules[ 42 ] + ")+";
 
+const rule11 = [];
+for ( let i = 1; i < 10; i++ ) {
+	rule11.push( "(" + rules[ 42 ] + "){" + i + "}(" + rules[ 31 ] + "){" + i + "}" );
+}
+rules[ 11 ] = "(" + rule11.join( "|" ) + ")";
+
+rules[ 0 ] = rules[ 8 ] + " " + rules[ 11 ];
+
+const theRule = new RegExp( "^" + rules[ 0 ].replace( /\s/g, "" ) + "$", "gm" );
 console.log( subjects.match( theRule ).length );
